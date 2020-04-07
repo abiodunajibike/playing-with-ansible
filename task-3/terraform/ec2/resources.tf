ae95b760ea1c9b737fa1b513f28b8a2e03bb00c0
Moruf Ajibike <moruf@evari.insure>
2020-04-07 08:32:55 +0100

    Initial commit

---
+++ task-3/terraform/ec2/resources.tf
@@ -0,0 +1,53 @@
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "DemoInstance"
+
+variable "instance_type" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "demo_sg_ids" {
+   type = list(string)
+}
+variable "ssh_key_name" {}
+variable "ssh_key_file_path" {}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = var.ssh_key_name
+  public_key = file(var.ssh_key_file_path)
+}
+
+resource "aws_ssm_parameter" "ec2_ssh_key" {
+  name  = "ec2_ssh_key"
+  type  = "SecureString"
+  value = file(var.ssh_key_file_path)
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  associate_public_ip_address = true
+  vpc_security_group_ids = var.demo_sg_ids
+  subnet_id = var.subnet_id
+  key_name = aws_key_pair.ssh_key.key_name
+
+  tags = {
+    Name = "DemoInstance"
+  }
+}
---
+++ task-3/terraform/main.tf
@@ -0,0 +1,22 @@
+
+module "networking" {
+  source = "./vpc"
+}
+
+module "security" {
+  source = "./security"
+
+  vpc_id              = module.networking.vpc_id
+}
+
+module "ec2" {
+  source = "./ec2"
+
+  instance_type = var.instance_type
+  vpc_id =  module.networking.vpc_id
+  subnet_id = module.networking.subnet_id
+  demo_sg_ids = module.security.demo_sg_ids
+  ssh_key_name = var.ssh_key_name
+  ssh_key_file_path = var.ssh_key_file_path
+}
+
